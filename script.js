8afb11539505b2c6857dad834273d6bce5f6bfd0
chill31 <89910163+chill31@users.noreply.github.com>
2024-07-19 14:34:35 +0530

    made menu functional and added themes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const theme = localStorage.getItem('theme') ?? 'green';
+if (localStorage.getItem('theme') === null) {
+  localStorage.setItem('theme', 'green');
+}
+document.body.style.setProperty('--accent', `var(--accent-${theme}`);
+document.querySelector(`.switch-${theme}`).classList.add('active');
+
 const mainTitle = document.querySelector('h1.name');
 const designation = document.querySelector('.designation');
 const designationSpans = document.querySelectorAll('.designation .letter');
@@ -18,7 +25,7 @@ observer.observe(designation);
 observer.observe(mainTitle);
 
 const randomChars = ["#", "$", "%", "&", "*", "+", "-", "!", "@", "?"];
-const actualDesignationContent = ['W', 'E', 'B', ' ', 'D', 'E', 'V', 'E', 'L', 'O', 'P', 'E', 'R'];
+const actualDesignationContent = 'web developer'.toUpperCase().split('');
 
 function animateDesignation() {
   designationSpans.forEach((span) => {
@@ -53,3 +60,88 @@ scrollIndicator.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+const projectsSection = document.querySelector('.showcase');
+const aboutMeSection = document.querySelector('.about-me');
+const footer = document.querySelector('footer');
+
+const menuLinks = document.querySelectorAll('.menu-link');
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.body.classList.toggle('menu-open');
+    disableMenuFocus();
+  });
+});
+
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', () => {
+  document.body.classList.toggle('menu-open');
+  if (document.body.classList.contains('menu-open')) {
+    menuLinks.forEach((link, index) => {
+      enableMenuFocus();
+      setTimeout(() => {
+        link.classList.add('visible-animate');
+      }, (index * 100) + 400);
+    });
+  } else {
+    menuLinks.forEach((link) => {
+      disableMenuFocus();
+      link.classList.remove('visible-animate');
+    });
+  }
+});
+
+const projectLinkButtons = document.querySelectorAll('.project-link-button');
+projectLinkButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    window.location.href = `https://${button.getAttribute('data-link')}.vercel.app`
+  });
+});
+
+const viewAllButton = document.querySelector('section.showcase button.github');
+viewAllButton.addEventListener('click', () => {
+  window.location.href = 'https://github.com/chill31?tab=repositories'
+});
+
+const menuGithubIcon = document.querySelector('.github-icon');
+menuGithubIcon.addEventListener('click', () => {
+  window.location.href = 'https://github.com/chill31'
+});
+
+const accentSwitcherSpans = document.querySelectorAll('.accent-switch');
+accentSwitcherSpans.forEach((span) => {
+  span.addEventListener('click', () => {
+
+    accentSwitcherSpans.forEach((span) => {
+      span.classList.remove('active');
+    });
+
+    const spanTheme = span.classList[1].replace('switch-', '');
+    localStorage.setItem('theme', spanTheme);
+    document.body.style.setProperty('--accent', `var(--accent-${spanTheme}`);
+    span.classList.add('active');
+
+  });
+});
+
+function enableMenuFocus() {
+  const focusable = document.querySelectorAll('button:not(.menu-button), a:not(.nav-logo)');
+  focusable.forEach((element) => {
+    element.setAttribute('tabindex', '-1');
+  });
+
+  menuLinks.forEach((link) => {
+    link.setAttribute('tabindex', '0');
+  });
+}
+
+function disableMenuFocus() {
+  const focusable = document.querySelectorAll('button, a');
+  focusable.forEach((element) => {
+    element.removeAttribute('tabindex');
+  });
+
+  menuLinks.forEach((link) => {
+    link.setAttribute('tabindex', '-1');
+  });
+}
